49be09a0342c5725996fd4c45f276766fd664022
marcoaam <marco.army@gmail.com>
2014-06-29 00:38:40 +0100

    RSPEC Practice calculator Final

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,28 +1,30 @@
 require "calculator"
- describe "calculates" do
+ describe "Simple Calculator" do
 
- 	context "Operations : " do
+ 	context "Operations : it can" do
 
-	 	it "sums two numbers" do
+	 	it "sum two numbers" do
 	 		expect(adds_two_numbers_given(2,2)).to eq (4)
-	 	end
-	 	it "Knows that 3 plus 5 is not 4" do
-	 		expect(adds_two_numbers_given(3, 5)).not_to eq (4)
 	 	end
 
 	 	it "substract two numbers" do
 	 		expect(substract_two_numbers_given(6,3)).to eq (3)
 	 	end
 
-		 it "multiplies two numbers" do
+		 it "multiply two numbers" do
 		 	expect(multiplies_two_numbers_given(4,4)).to eq (16)
 		 end
 
-	 	 it "divides two numbers" do
+	 	 it "divide two numbers" do
 		 	expect(divides_two_numbers_given(6,3)).to eq(2)
 		 end
 	end
 	context "Knows that" do
+
+		it "3 plus 5 is not 4" do
+	 		expect(adds_two_numbers_given(3, 5)).not_to eq (4)
+	 	end
+
 	 	it '7 minus 6 is not 3' do
 	 		expect(substract_two_numbers_given(7, 6)).not_to eq (3)
 	 	end
@@ -30,10 +32,10 @@ require "calculator"
 		 it "5 multiplied by 5 is not 16" do
 		 	expect(multiplies_two_numbers_given(5, 5)).not_to eq (16)
 		 end
-		 it "30 divided by 2" do
+		 it "30 divided by 2 is not 2" do
 		 	expect(divides_two_numbers_given(30, 2)).not_to eq (2)
 		 end
-		 it "a number divided by zero is not possible" do
+		 it "A number divided by zero is not possible" do
 		 	expect(divides_two_numbers_given(2,0)).to eq "Invalid operation"
 		end
 	end
